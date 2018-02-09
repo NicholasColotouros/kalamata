f96ab820ee238eb80a294aa374898968897bb899
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2018-02-08 21:45:45 -0500

    Created a scene with the board and started camera control script

---
+++ kalamata/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform cameraPivot;
+
+    // For zooming
+    public float minDist;
+    public float maxDist;
+
+    // For rotating vertically
+    public float minXRotation = 0;
+    public float maxXRotation = 90;
+
+    private float currentRotation = 0;
+    private float currentDistance = 0;
+
+    // Use this for initialization
+    void Start () {
+        transform.LookAt(transform.parent.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO Start with right click + mouse rotation and then throw in rewired
+	}
+}
