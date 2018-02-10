b55dc33745c164acfa97c1b6f3c8f3cc44a76b79
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2018-02-10 09:20:18 -0500

    added zoom and right click to rotate

--- kalamata/Assets/Scripts/CameraController.cs
+++ kalamata/Assets/Scripts/CameraController.cs
@@ -8,6 +8,9 @@ public class CameraController : MonoBehaviour
     public float minVerticalRotation = 15;
     public float maxVerticalRotation = 80;
 
+    public float minDistance = 5;
+    public float maxDistance = 15;
+
     private Transform cameraPivot;
     private Player player;
     
@@ -24,13 +27,31 @@ public class CameraController : MonoBehaviour
 	
 	void Update ()
     {
-        // TODO Start with right click + mouse rotation and then throw in rewired
-        float horizontalRot = player.GetAxis("Rotate Horizontal");
-        float verticalRot = player.GetAxis("Rotate Vertical");
+        ProcessRotationInput();
+        ProcessZoomInput();
+    }
 
-        Vector3 currentRotation = cameraPivot.eulerAngles;
-        currentRotation += new Vector3(verticalRot, horizontalRot, 0);
-        currentRotation.x = Mathf.Clamp(currentRotation.x, minVerticalRotation, maxVerticalRotation);
-        cameraPivot.eulerAngles = currentRotation;
+    void ProcessRotationInput()
+    {
+        if (player.GetButtonTimedPress("ActivateRotation", 0))
+        {
+            float horizontalRot = player.GetAxis("Rotate Horizontal");
+            float verticalRot = player.GetAxis("Rotate Vertical");
+
+
+            Vector3 currentRotation = cameraPivot.eulerAngles;
+            currentRotation += new Vector3(verticalRot, horizontalRot, 0);
+            currentRotation.x = Mathf.Clamp(currentRotation.x, minVerticalRotation, maxVerticalRotation);
+            cameraPivot.eulerAngles = currentRotation;
+        }
+    }
+
+    void ProcessZoomInput()
+    {
+        // Camera is on the -Z so we invert
+        Vector3 localposition = transform.localPosition;
+        localposition.z = Mathf.Clamp(localposition.z += player.GetAxis("Zoom"), -maxDistance, -minDistance);
+
+        transform.localPosition = localposition;
     }
 }
