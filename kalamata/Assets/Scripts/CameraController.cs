8d228397e1b21321564a5d93dd13cb4bd01b342c
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2018-02-09 22:27:23 -0500

    camera rotates around board

--- kalamata/Assets/Scripts/CameraController.cs
+++ kalamata/Assets/Scripts/CameraController.cs
@@ -1,28 +1,36 @@
-﻿using System.Collections;
+﻿using Rewired;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-    public Transform cameraPivot;
+public class CameraController : MonoBehaviour
+{
+    public float minVerticalRotation = 15;
+    public float maxVerticalRotation = 80;
 
-    // For zooming
-    public float minDist;
-    public float maxDist;
+    private Transform cameraPivot;
+    private Player player;
+    
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
 
-    // For rotating vertically
-    public float minXRotation = 0;
-    public float maxXRotation = 90;
-
-    private float currentRotation = 0;
-    private float currentDistance = 0;
-
-    // Use this for initialization
-    void Start () {
-        transform.LookAt(transform.parent.transform.position);
+    void Start ()
+    {
+        cameraPivot = transform.parent.transform;
+        transform.LookAt(cameraPivot.transform);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		// TODO Start with right click + mouse rotation and then throw in rewired
-	}
+	void Update ()
+    {
+        // TODO Start with right click + mouse rotation and then throw in rewired
+        float horizontalRot = player.GetAxis("Rotate Horizontal");
+        float verticalRot = player.GetAxis("Rotate Vertical");
+
+        Vector3 currentRotation = cameraPivot.eulerAngles;
+        currentRotation += new Vector3(verticalRot, horizontalRot, 0);
+        currentRotation.x = Mathf.Clamp(currentRotation.x, minVerticalRotation, maxVerticalRotation);
+        cameraPivot.eulerAngles = currentRotation;
+    }
 }
